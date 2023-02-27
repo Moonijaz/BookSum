0c4953ad4deea8a895806788d776b632d46f0adb
Alina-Ijaz <alinaijaz073@gmail.com>
2023-02-27 08:07:23 +0500

    Fix identation

--- my-app/src/Component/Left_body.js
+++ my-app/src/Component/Left_body.js
@@ -3,12 +3,12 @@
 function Image() {
   return (
     <div className="left">
-            <div className="overlay">
-                <img src="./Photo by Janko Ferlic on Pexels.jfif" width="550px" alt="Photo by Janko Ferlic on Pexels"/>
-            </div>
-        </div>
+      <div className="overlay">
+        <img src="./Photo by Janko Ferlic on Pexels.jfif" width="550px" alt="Photo by Janko Ferlic on Pexels" />
+      </div>
+    </div>
 
-    
+
   );
 }
 
