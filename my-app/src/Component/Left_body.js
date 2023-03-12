eb7d045efc155cd768f9602e9ddafee40702a8e2
Alina-Ijaz <alinaijaz073@gmail.com>
2023-03-12 18:39:26 +0500

    Add app.js component

---
+++ my-app/src/Component/Left_body.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="left">
+            <div className="overlay">
+                <img src="./Photo by Janko Ferlic on Pexels.jfif" width="550px"/>
+            </div>
+        </div>
+
+    
+  );
+}
+
+export default App;
