717739831caf696f4b4e7d8c1832e3a7aeac120f
Moon ijaz <moonijaz006@gmail.com>
2023-03-13 22:02:23 +0500

    Add image in left body

--- my-app/src/Component/Left_body.js
+++ my-app/src/Component/Left_body.js
@@ -4,7 +4,7 @@ function Image() {
   return (
     <div className="left">
             <div className="overlay">
-                <img src="./Photo by Janko Ferlic on Pexels.jfif" width="550px" alt="background image"/>
+                <img src="./Photo by Janko Ferlic on Pexels.jfif" width="550px" alt="Photo by Janko Ferlic on Pexels"/>
             </div>
         </div>
 
